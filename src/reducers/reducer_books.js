8ce7c37e11feda47ac9f17252ec961e34967bdc1
kazi <kazi-Imran.Aziz@adviqo.com>
2017-08-09 13:35:38 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,19 @@
+export default function() {
+  return [
+    {  title: 'Javascript:The Good parts'
+    },
+    {
+      title: 'Harry Potter'
+    },
+    {
+      title: 'The Dark Tower'
+    },
+    {
+      title: 'Eloquent Ruby'
+    }
+
+  ]
+
+
+
+}
